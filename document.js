efaec711d28a58da2024d0ab2acddb0717c83235
doujin5 <abhi.monty143@gmail.com>
2018-03-06 20:22:12 +0530

    ghgf

--- document.js
+++ document.js
@@ -1,42 +1,20 @@
 var express = require("express");
-var app = express();
-var router=express.Router();
-var port = 3000;
- var Mongo = require("mongodb").MongoClient;
- var assert=require('assert');
-Mongo.connect("mongodb://localhost:27017/first");
-router.get('/',function(req,res,next){
-	res.render('document');
-});
-router.get('/test3',function(req,res,next){
-	var result=[];
- assert.equal(null,err);
- var cursor=db.collection('user_data').find();
- cursor.forEach(function(doc,err){
- 	assert.equal(null,err);
- 	result.push(doc);
- 	},function(){
- 		db.close();
- 		res.render('document', {logindata : result});
- 	}); 
- });
-router.post('/login',function(req,res,next){
-var logindata ={
-	mail_id : req.body.mail_id,
-	password : req.body.password,
-}; 
-assert.equal(null,err);
-db.collection('user_data').insertOne(logindata,function(err,result){
-	assert.equal(null,err);
-	console.log('items inserted');
-	db.close();
+var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
 
-})  
-});
-app.use("/", (req, res) => {
-  res.sendFile(__dirname + "/style.html");
-});
- 
+var app = express();
+app.use(bodyParser.json());
+// mongoose.connect('mongodb://localhost/login');
+// mongoose.Promise=global.Promise;
+var api = require('./routes/api.js');
+
+var port = 3000;
+app.use(express.static('public'))
+app.use("/api",api);
+
+ app.use("/data",(req,res)=>{
+ 	res.sendFile(__dirname+"/public/test3.html");
+ })
 app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
